d52e26d4938ca8e3e7378c8ba5ce5c768ebc1779
Eduardo Afán <eduardoafan.eae@gmail.com>
2023-12-28 13:43:47 +0100

    Update

--- src/Tema4_Arrays_Base/Ejercicio7.java
+++ src/Tema4_Arrays_Base/Ejercicio7.java
@@ -12,40 +12,77 @@ Una vez hecho esto saldrá un menú que me dará las siguientes opciones:
     - Ver palabra con más letras
     - Ver palabra con menos letras*/
 public class Ejercicio7 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        /*Crea un programa que pida 10 palabras por consola y las guarde en un array de String.*/
         System.out.println("Introduce 10 palabras : ");
         String diezPalabras = scanner.nextLine();
-        /*mostrarla todas las palabras del array*/
         String[] frase = diezPalabras.split(" ");
-        System.out.println("---------------------------------------------------");
-        System.out.println();
-        /*Obtener una palabra al azar: mostrará una palabra de las que existen en el array*/
-        for (String frases : frase) {
-            System.out.print(frases + " ");
-        }
-        Random random = new Random();
-        int palabraRandom = random.nextInt(frase.length);
-        System.out.println("\nPalabra al azar : " + frase[palabraRandom]);
-        /* ¿Cuantas palabras hay en la frase? */
-        System.out.println("\nCantidadd de palabras en la frase : " + frase.length);
-        /*mostrará el número de letras que hay en total entre todas las palabras*/
-        int totalLetras = 0;
-        for (String palabra : frase) {
-            for (int i = 0; i < palabra.length(); i++) {
-                totalLetras++;
+        int opcion;
+        do {
+            System.out.println("1. Ver todas las plabras ");
+            System.out.println("2. Obtener una palabra al azar ");
+            System.out.println("3. Ver número de letras ");
+            System.out.println("4. Ver media de letras ");
+            System.out.println("5. Ver palabra con mas letras ");
+            System.out.println("6. Ver palabra con menos letras");
+            System.out.println("7. Salir del programa ");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+            switch (opcion) {
+                case 1:
+                    if (frase.length == 10) {
+                        System.out.println("La frase es : \n " + diezPalabras);
+                    } else {
+                        System.out.println("Debe tener 10 palabras");
+                    }
+                    break;
+                case 2:
+                    Random random = new Random();
+                    int palabrarandom = random.nextInt(frase.length);
+                    System.out.println("La palabra al azar es : \n" + frase[palabrarandom]);
+                    break;
+                case 3:
+                    int numeroLetras = 0;
+                    for (String palabra : frase) {
+                        numeroLetras += palabra.length();
+                    }
+                    System.out.println("El numero de letras es : " + numeroLetras);
+                    break;
+                case 4:
+                    double mediaLetras = 0;
+                    for (String palabra : frase) {
+                        mediaLetras += palabra.length();
+                    }
+                    mediaLetras /= frase.length;
+                    System.out.printf("La Media de letras es : %.2f%n", mediaLetras);
+                    break;
+                case 5:
+                    String palabraMayor = "";
+                    for (String palabra : frase) {
+                        if (palabra.length() > palabraMayor.length()) {
+                            palabraMayor = palabra;
+                        }
+                    }
+                    System.out.println("La palabra mayor es : \n" + palabraMayor);
+                    break;
+                case 6:
+                    String palabraMenor = frase[0];
+                    for (String palabra : frase) {
+                        if (palabra.length() < palabraMenor.length()) {
+                            palabraMenor = palabra;
+                        }
+                    }
+                    System.out.println("La palabra menos es : \n" + palabraMenor);
+                    break;
             }
         }
-        System.out.println("\ntotal de letras que hay en la frase : " + totalLetras);
-        /* mostrará el número medio de letras entre todas las palabas */
-        double mediapalabras = ((double) totalLetras++ / frase.length);
-        System.out.printf("Dime la media de letras por palabra : %.2f\n", mediapalabras);
-        /* Cuantas palabras tengo, ejemplo clase*/
-        String palabras= "Todo esta incluido";
-        int numeropalabras = palabras.split(" ").length;
-        System.out.println("el número de palabras es " +numeropalabras);
-        /*ver palabra con más letras */
+        while (opcion != 7);
+        System.out.println("\"Sayonara Baby\"");
+
 
     }
 }
+
+
+
