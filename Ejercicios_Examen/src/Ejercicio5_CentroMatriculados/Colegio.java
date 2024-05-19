adfec4f45583969c3520ee5d456df64fe44d50b6
Eduardo Afán <eduardoafan.eae@gmail.com>
2024-05-19 12:31:27 +0200

    Update

--- Ejercicios_Examen/src/Ejercicio5_CentroMatriculados/Colegio.java
+++ Ejercicios_Examen/src/Ejercicio5_CentroMatriculados/Colegio.java
@@ -18,8 +18,8 @@ public class Colegio {
 
     public void darAlta(Alumno alumno) {
 
-       ++contadorMatriculas;
-       alumno.setnMatricula(contadorMatriculas);
+
+       alumno.setnMatricula(contadorMatriculas++);
        listaAlumnos.add(alumno);
         System.out.println("Alumno añadido correctamente ");
 
@@ -28,24 +28,18 @@ public class Colegio {
 
     public void calificarAlumno(int nMatricula,double calificacion){
 
-        boolean alumnoEncontrado=false;
+
 
        for (Alumno alumno : listaAlumnos){
 
            if (alumno.getnMatricula() ==nMatricula){
                alumno.setCalificacion(calificacion);
                System.out.println("Alumno calificado correctamente ");
-               alumnoEncontrado=true;
-break;
-
-           }
-       }if(!alumnoEncontrado){
-            System.out.println("Nose encontro alumno");
-        }
 
 
 
-    }
+
+    }}}
 
     public ArrayList<Alumno> getListaAlumnos() {
         return listaAlumnos;
